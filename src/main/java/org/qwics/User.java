a16f552fc1ddf07334f45f02191baa4da3fb0a5a
Gernot Schulmeister <gernot.schulmeister@kmon.net>
2025-02-08 06:51:49 +0100

    use lombok

--- src/main/java/org/qwics/User.java
+++ src/main/java/org/qwics/User.java
@@ -2,34 +2,12 @@ package org.qwics;
 
 
 import java.io.Serializable;
+import lombok.Data;
 
+@Data
 public class User implements Serializable {
 
   private String vorname;
   private String nachname;
   private String address;
-
-  public String getVorname() {
-    return vorname;
-  }
-
-  public void setVorname(String vorname) {
-    this.vorname = vorname;
-  }
-
-  public String getNachname() {
-    return nachname;
-  }
-
-  public void setNachname(String nachname) {
-    this.nachname = nachname;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
 }
